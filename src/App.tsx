a3415ef3480330477041273a7cea7e96ee67440b
Nishad Sheriffdeen <nishadsheriffdeen@icloud.com>
2024-04-25 23:46:08 +0530

    fix : route updated

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/predict" element={<UploadImagePage />} />
+      <Route path="/demo" element={<UploadImagePage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
